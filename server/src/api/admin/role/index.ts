39f162ca51c55b841b413fddf0c70c281cc3f15f
soumitradev <soumitra.shewale.dev@gmail.com>
2025-01-28 03:42:20 +0530

    feat: implement edit role permissions endpoint

--- server/src/api/admin/role/index.ts
+++ server/src/api/admin/role/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import createRouter from "./create";
 import getAllRouter from "./all";
 import deleteRouter from "./delete";
+import editRouter from "./edit";
 const router = express.Router();
 
 router.use("/create", createRouter);
 router.use("/all", getAllRouter);
 router.use("/delete", deleteRouter);
+router.use("/edit/:role", editRouter);
 
 export default router;
