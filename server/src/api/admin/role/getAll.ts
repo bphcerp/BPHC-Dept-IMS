cfa679173b5edd94cbc26211d7227323da4c3f8b
soumitradev <soumitra.shewale.dev@gmail.com>
2025-01-28 00:30:23 +0530

    refactor: refactor get all roles endpoint to use sql-like format

--- server/src/api/admin/role/getAll.ts
+++ server/src/api/admin/role/getAll.ts
@@ -2,6 +2,7 @@ import { asyncHandler } from "@/middleware/routeHandler";
 import assert from "assert";
 import express from "express";
 import { checkAccess } from "@/middleware/auth";
+import { roles } from "@/config/db/schema/admin";
 import db from "@/config/db";
 const router = express.Router();
 
@@ -11,8 +12,13 @@ router.get(
     asyncHandler(async (req, res, _) => {
         assert(req.user);
 
-        const allRoles = await db.query.roles.findMany();
-        res.json(allRoles.map((x) => x.role));
+        const allRoles = await db
+            .select({
+                role: roles.role,
+            })
+            .from(roles)
+            .execute();
+        res.json(allRoles.map((role) => role.role));
     })
 );
 
