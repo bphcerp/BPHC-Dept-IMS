99853eed693bfec5c9d2808f7daf2aa1ba0f442d
soumitradev <soumitra.shewale.dev@gmail.com>
2025-01-28 03:03:56 +0530

    feat: implement get role permissions endpoint

---
+++ server/src/api/admin/role/get.ts
@@ -0,0 +1,48 @@
+import { asyncHandler } from "@/middleware/routeHandler";
+import assert from "assert";
+import express from "express";
+import { checkAccess } from "@/middleware/auth";
+import z from "zod";
+import db from "@/config/db";
+import { roles } from "@/config/db/schema/admin";
+import { HttpCode, HttpError } from "@/config/errors";
+import { eq } from "drizzle-orm";
+
+const router = express.Router();
+
+const pathSchema = z.object({
+    role: z
+        .string()
+        .trim()
+        .nonempty()
+        .regex(/^[a-z0-9-]+$/)
+        .max(128),
+});
+
+router.post(
+    "/",
+    checkAccess("role:read"),
+    asyncHandler(async (req, res, next) => {
+        assert(req.user);
+        const parsed = pathSchema.parse(req.params);
+        const role = await db
+            .select()
+            .from(roles)
+            .where(eq(roles.role, parsed.role))
+            .limit(1)
+            .execute();
+
+        if (role.length === 0) {
+            return next(
+                new HttpError(
+                    HttpCode.NOT_FOUND,
+                    `Role '${parsed.role}' not found`
+                )
+            );
+        }
+
+        res.json(role[0]);
+    })
+);
+
+export default router;
--- server/src/api/admin/role/index.ts
+++ server/src/api/admin/role/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import createRouter from "./create";
 import getAllRouter from "./all";
 import deleteRouter from "./delete";
+import getPermissionsRouter from "./get";
 const router = express.Router();
 
 router.use("/create", createRouter);
 router.use("/all", getAllRouter);
 router.use("/delete", deleteRouter);
+router.use("/:role", getPermissionsRouter);
 
 export default router;
