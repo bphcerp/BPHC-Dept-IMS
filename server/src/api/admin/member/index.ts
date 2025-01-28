f72c6554b217fb312f483f7ccb6ab01fc9eba816
kishan1735 <gswwarriorsnba@gmail.com>
2025-01-28 22:53:10 +0530

    feat: add deactivate user endpoint

--- server/src/api/admin/member/index.ts
+++ server/src/api/admin/member/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import inviteRouter from "./invite";
 import editRolesRouter from "./edit";
 import searchRouter from "./search";
+import deactivateRouter from "./deactivate";
 
 const router = express.Router();
 
 router.use("/invite", inviteRouter);
 router.use("/edit", editRolesRouter);
 router.use("/search", searchRouter);
+router.use("/deactivate", deactivateRouter);
 
 export default router;
