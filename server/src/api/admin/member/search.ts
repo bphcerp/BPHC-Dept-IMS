4a6ea6859607e60ecd7a465e13bc08738b6a52b5
kishan1735 <gswwarriorsnba@gmail.com>
2025-01-28 17:54:38 +0530

    fix: minor fixes

--- server/src/api/admin/member/search.ts
+++ server/src/api/admin/member/search.ts
@@ -15,28 +15,17 @@ const searchSchema = z.object({
 router.post(
     "/",
     asyncHandler(async (req, res, next) => {
-        const parseResults = searchSchema.safeParse(req.body);
-        try {
-            const results = await db
-                .select()
-                .from(users)
-                .where(
-                    or(
-                        ilike(
-                            users.email,
-                            `%${parseResults.data?.searchQuery}%`
-                        ),
-                        sql`lower(${parseResults.data?.searchQuery}) = ANY(users.roles)`
-                    )
-                );
-            res.status(200).json({ success: true, searchResults: results });
-        } catch (err) {
-            throw new HttpError(
-                HttpCode.INTERNAL_SERVER_ERROR,
-                "Error while searching for members",
-                (err as Error)?.message
+        const { searchQuery } = searchSchema.parse(req.body);
+        const results = await db
+            .select()
+            .from(users)
+            .where(
+                or(
+                    ilike(users.email, `%${searchQuery}%`),
+                    sql`lower(${searchQuery}) = ANY(users.roles)`
+                )
             );
-        }
+        res.status(200).json({ success: true, searchResults: results });
     })
 );
 
