29983af798495113b5e927af5eb31b63f1e4610e
soumitradev <soumitra.shewale.dev@gmail.com>
2025-01-28 05:59:15 +0530

    feat: add demo usage of lib functions with typescript on frontend

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "@/hooks/Auth";
 import api from "@/lib/axios-instance";
 import { LOGIN_ENDPOINT } from "@/lib/constants";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { libTest } from "lib";
 
 function Home() {
   const { authState, updateAuthState, logOut } = useAuth();
@@ -27,6 +28,9 @@ function Home() {
         <div className="flex flex-col gap-4">
           <p className="whitespace-pre text-left font-mono">
             {JSON.stringify(authState, null, 4)}
+          </p>
+          <p>
+            {libTest("client")}
           </p>
           <button className="self-center" onClick={() => logOut()}>
             Logout
