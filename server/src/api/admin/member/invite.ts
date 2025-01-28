bf0bc8cf043a6e8b73af5a990561b6bd272b89be
Shiv Patil <krymzin0311@gmail.com>
2025-01-28 21:30:23 +0530

    fix(server): invite member endpoint

--- server/src/api/admin/member/invite.ts
+++ server/src/api/admin/member/invite.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { z } from "zod";
 import db from "@/config/db";
-import { users } from "@/config/db/schema/admin";
+import { faculty, phd, users, userType } from "@/config/db/schema/admin";
 import { HttpError, HttpCode } from "@/config/errors";
 import { asyncHandler } from "@/middleware/routeHandler";
 import nodemailer from "nodemailer";
@@ -10,53 +10,102 @@ import { checkAccess } from "@/middleware/auth";
 
 const router = express.Router();
 
-const addMemberBodySchema = z.object({
-    email: z.string().email(),
-});
+const bodySchema = z.intersection(
+    z.object({
+        email: z.string().email(),
+        name: z.string().trim().nonempty(),
+        psrn: z.string().trim().nonempty(),
+        phone: z.string().trim().optional(),
+        department: z.string().trim().optional(),
+    }),
+    z.discriminatedUnion("type", [
+        z.object({
+            type: z.literal(userType.enumValues[0]), // Faculty
+            designation: z.string().trim().optional(),
+            room: z.string().trim().optional(),
+        }),
+        z.object({
+            type: z.literal(userType.enumValues[1]), // PhD
+        }),
+    ])
+);
 
 router.post(
     "/",
     checkAccess("member:invite"),
     asyncHandler(async (req, res, next) => {
-        const { email } = addMemberBodySchema.parse(req.body);
+        const parsed = bodySchema.parse(req.body);
         try {
             await db.transaction(async (db) => {
                 // Insert the user into the database
-                const inserted = await db
+                const insertedUser = await db
                     .insert(users)
                     .values({
-                        email,
+                        email: parsed.email,
+                        name: parsed.name,
+                        type: parsed.type,
                         roles: [], // Default to an empty array for roles
                     })
                     .onConflictDoNothing()
                     .returning();
-                if (inserted.length === 0) {
+                if (insertedUser.length === 0) {
                     throw new HttpError(
                         HttpCode.CONFLICT,
                         "User already exists"
                     );
                 }
+                // Insert user details
+                let insertedDetails;
+                if (parsed.type === userType.enumValues[0]) {
+                    insertedDetails = await db
+                        .insert(faculty)
+                        .values({
+                            psrn: parsed.psrn,
+                            email: insertedUser[0].email,
+                            department: parsed.department,
+                            designation: parsed.designation,
+                            room: parsed.room,
+                            phone: parsed.phone,
+                        })
+                        .onConflictDoNothing()
+                        .returning();
+                } else {
+                    insertedDetails = await db
+                        .insert(phd)
+                        .values({
+                            psrn: parsed.psrn,
+                            email: insertedUser[0].email,
+                            phone: parsed.phone,
+                        })
+                        .onConflictDoNothing()
+                        .returning();
+                }
+                if (insertedDetails.length === 0) {
+                    throw new HttpError(HttpCode.CONFLICT, "Duplicate PSRN");
+                }
                 // Send invitation email
-                try {
-                    const transporter = nodemailer.createTransport({
-                        service: "gmail",
-                        auth: {
-                            user: env.BPHCERP_EMAIL,
-                            pass: env.BPHCERP_PASSWORD,
-                        },
-                    });
-                    await transporter.sendMail({
-                        from: env.BPHCERP_EMAIL,
-                        to: email,
-                        subject: "Member invitation",
-                        text: `Hello! You are invited to access the EEE ERP portal. Website link: ${env.FRONTEND_URL}`,
-                    });
-                } catch (e) {
-                    throw new HttpError(
-                        HttpCode.INTERNAL_SERVER_ERROR,
-                        "Member invitation failed: error sending invitation email",
-                        (e as Error)?.message
-                    );
+                if (env.PROD) {
+                    try {
+                        const transporter = nodemailer.createTransport({
+                            service: "gmail",
+                            auth: {
+                                user: env.BPHCERP_EMAIL,
+                                pass: env.BPHCERP_PASSWORD,
+                            },
+                        });
+                        await transporter.sendMail({
+                            from: env.BPHCERP_EMAIL,
+                            to: parsed.email,
+                            subject: "Member invitation",
+                            text: `Hello! You are invited to access the EEE ERP portal. Website link: ${env.FRONTEND_URL}`,
+                        });
+                    } catch (e) {
+                        throw new HttpError(
+                            HttpCode.INTERNAL_SERVER_ERROR,
+                            "Member invitation failed: error sending invitation email",
+                            (e as Error)?.message
+                        );
+                    }
                 }
                 return res.status(200).json({ success: true });
             });
