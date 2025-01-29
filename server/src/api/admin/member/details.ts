74ef7cba005ce4737043d2a9426a1bbfaae1884f
achaldps@gmail.com <achaldps@gmail.com>
2025-01-29 16:45:55 +0530

    feat: optimized user details endpoint by decreasing one query and added db relations

--- server/src/api/admin/member/details.ts
+++ server/src/api/admin/member/details.ts
@@ -3,7 +3,7 @@ import express from "express";
 import db from "@/config/db";
 import { checkAccess } from "@/middleware/auth";
 import { z } from "zod";
-import { users, faculty, phd } from "@/config/db/schema/admin";
+import { users } from "@/config/db/schema/admin";
 import { HttpCode, HttpError } from "@/config/errors";
 import { eq } from "drizzle-orm";
 const router = express.Router();
@@ -17,39 +17,16 @@ router.get(
     checkAccess("member:read"),
     asyncHandler(async (req, res, next) => {
         const parsed = querySchema.parse(req.body);
-        const user = await db
-            .select()
-            .from(users)
-            .where(eq(users.email, parsed.email))
-            .limit(1);        
-        if (!user || user.length === 0) {
-            return next(
-                new HttpError(HttpCode.NOT_FOUND, "User not found")
-            );
+        const user = await db.query.users.findMany({
+            where: eq(users.email, parsed.email),
+            with: {
+                faculty: true,
+                phd: true,
+            }
+        })
+        if (user.length === 0) {
+            return next(new HttpError(HttpCode.NOT_FOUND, "User not found"));
         }
-        // check if user is faculty or phd
-        
-        if (user[0].type === "faculty") {
-            const facultyUser = await db
-                .select()
-                .from(faculty)
-                .where(eq(faculty.email, parsed.email))
-                .limit(1);
-
-            // extend user object with faculty details
-            user[0] = { ...user[0], ...facultyUser[0] };
-
-        } else if (user[0].type === "phd") {
-            const phdUser = await db
-                .select()
-                .from(phd)
-                .where(eq(phd.email, parsed.email))
-                .limit(1);
-
-            // extend user object with phd details
-            user[0] = { ...user[0], ...phdUser[0] };
-        }
-
         res.status(200).json(user[0]);
     })
 );
