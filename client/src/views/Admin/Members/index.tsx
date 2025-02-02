d748ea8b7bf447e608c97819ba09a1196edee749
Shiv Patil <krymzin0311@gmail.com>
2025-02-02 23:15:52 +0530

    feat(client): User list page + component

---
+++ client/src/views/Admin/Members/index.tsx
@@ -0,0 +1,43 @@
+import MemberList, { type Member } from "@/components/admin/MemberList";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LoadingSpinner } from "@/components/ui/spinner";
+import api from "@/lib/axios-instance";
+import { useQuery } from "@tanstack/react-query";
+import { Search } from "lucide-react"
+import { useState } from "react";
+
+const MembersView = () => {
+    const [search, setSearch] = useState("");
+
+    const { data: members, isFetching, refetch } = useQuery({
+        queryKey: ["members"],
+        queryFn: async () => {
+            const response = await api.get<Member[]>("/admin/member/search", {
+                params: {
+                    q: search
+                }
+            });
+            return response.data;
+        },
+        refetchOnWindowFocus: false,
+        staleTime: Infinity
+    })
+
+    return (
+        <div className="mx-auto flex max-w-5xl flex-1 flex-col justify-center gap-4 p-4">
+            <h1 className="text-3xl font-bold text-primary">Members</h1>
+            <div className="flex items-center gap-4">
+                <div className="relative">
+                    <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                    <Input type="search" placeholder="Search members..." value={search} onChange={e => setSearch(e.target.value)} className="pl-9 w-64" />
+                </div>
+                <Button onClick={() => void refetch()}>Search</Button>
+            </div>
+            {isFetching ? <LoadingSpinner /> : !members?.length ? <p>No members found</p> : <MemberList members={members} />}
+
+        </div>
+    );
+};
+
+export default MembersView;
--- client/src/views/Admin/index.tsx
+++ client/src/views/Admin/index.tsx
@@ -1,8 +1,8 @@
+import { Navigate } from "react-router-dom";
+
 const Admin = () => {
   return (
-    <div className="mx-auto flex max-w-5xl flex-1 flex-col items-center justify-center gap-8 p-8 text-center">
-      ADMIN
-    </div>
+    <Navigate to="/admin/members" />
   );
 };
 
