808b53191404e4e0c14b96574d4301f14f50b099
Shiv Patil <krymzin0311@gmail.com>
2025-02-03 15:33:50 +0530

    feat(server): search query in role/all endpoint

--- server/src/api/admin/role/all.ts
+++ server/src/api/admin/role/all.ts
@@ -1,23 +1,28 @@
 import { asyncHandler } from "@/middleware/routeHandler.ts";
-import assert from "assert";
 import express from "express";
 import { checkAccess } from "@/middleware/auth.ts";
-import { roles } from "@/config/db/schema/admin.ts";
 import db from "@/config/db/index.ts";
+import { z } from "zod";
 const router = express.Router();
+
+const querySchema = z.object({
+    q: z.string().trim().optional(),
+});
 
 router.get(
     "/",
     checkAccess("role:read"),
     asyncHandler(async (req, res, _) => {
-        assert(req.user);
-
-        const allRoles = await db
-            .select({
-                role: roles.role,
-            })
-            .from(roles)
-            .execute();
+        const { q: searchQuery } = querySchema.parse(req.query);
+        const allRoles = await db.query.roles.findMany({
+            columns: {
+                role: true,
+            },
+            where: (fields, { ilike }) =>
+                searchQuery?.length
+                    ? ilike(fields.role, `%${searchQuery}%`)
+                    : undefined,
+        });
         res.json(allRoles.map((role) => role.role));
     })
 );
