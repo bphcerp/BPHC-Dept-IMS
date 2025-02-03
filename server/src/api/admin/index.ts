59535354bea81796a647f8ce822ba30084a89299
Sanchay <sanchaynts9422@gmail.com>
2025-02-04 01:55:09 +0530

    cleanup phd

--- server/src/api/admin/index.ts
+++ server/src/api/admin/index.ts
@@ -2,13 +2,12 @@ import express from "express";
 import roleRouter from "./role/index.ts";
 import memberRouter from "./member/index.ts";
 import permissionRouter from "./permission/index.ts";
-import phdRouter from "./phd/index.ts"
+
 
 const router = express.Router();
 
 router.use("/role", roleRouter);
 router.use("/member", memberRouter);
 router.use("/permission", permissionRouter);
-router.use("/phd", phdRouter);
 
 export default router;
--- server/src/api/admin/phd/index.ts
+++
@@ -1,9 +0,0 @@
-import inputDetails from "./inputDetails.ts"
-import express from "express";
-
-
-const router = express.Router();
-
-router.use("/inputDetails", inputDetails);
-
-export default router;
