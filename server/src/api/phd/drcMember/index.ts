5bdb32b9b1b5384ab2f69fdeb64b575eebd98091
Sanchay <sanchaynts9422@gmail.com>
2025-02-05 00:53:03 +0530

    refractor(phd): cleanup with qualifying dates

--- server/src/api/phd/drcMember/index.ts
+++ server/src/api/phd/drcMember/index.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import getPhDRecords from "./getPhDRecords.ts";
 import updateExamDates from "./updateExamDates.ts";
-import updateQualifyingExam from "./updateQualifyingExam.ts";
 import updateExamRouter from '../drcMember/updateExam.ts';
 const router = express.Router();
 
 router.use("/get-phd", getPhDRecords);
 router.use("/update-exam-dates", updateExamDates);
-router.use("/update-qualifying-exam", updateQualifyingExam);
 router.use('/update-exam', updateExamRouter);
 
 export default router;
