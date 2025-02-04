3bae82e39cdaaa8988fe18397cfe0fe0251e6713
Sanchay <sanchaynts9422@gmail.com>
2025-02-05 01:13:22 +0530

    feat(drc): get Faculty Details endpoint

---
+++ server/src/api/phd/drcMember/getFacultyDetails.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { asyncHandler } from "@/middleware/routeHandler.ts";
+import { checkAccess } from "@/middleware/auth.ts";
+import db from "@/config/db/index.ts";
+import { faculty } from "@/config/db/schema/admin.ts";
+
+const router = express.Router();
+
+router.get(
+    "/faculty-names",
+    checkAccess("drc-viewFaculty"),
+    asyncHandler(async (_req, res) => {
+        const facultyList = await db
+            .select({
+                email: faculty.email,
+                name: faculty.name,
+            })
+            .from(faculty);
+
+        res.json({ success: true, faculty: facultyList });
+    })
+);
+
+export default router;
--- server/src/api/phd/drcMember/index.ts
+++ server/src/api/phd/drcMember/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import getPhDRecords from "./getPhDRecords.ts";
 import updateExamDates from "./updateExamDates.ts";
-import updateExamRouter from '../drcMember/updateExam.ts';
+import updateExamRouter from './updateExam.ts';
+import getFacultyDetails from "./getFacultyDetails.ts"
 const router = express.Router();
 
 router.use("/get-phd", getPhDRecords);
 router.use("/update-exam-dates", updateExamDates);
 router.use('/update-exam', updateExamRouter);
+router.use('/get-faculty-details', getFacultyDetails);
 
 export default router;
