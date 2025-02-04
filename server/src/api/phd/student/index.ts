0185f933724958e549d075221a30dc26b6bd5f62
Sanchay <sanchaynts9422@gmail.com>
2025-02-05 01:28:30 +0530

    feat(phd): check/verify exam status of phd

--- server/src/api/phd/student/index.ts
+++ server/src/api/phd/student/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { authMiddleware } from '@/middleware/auth.ts';
 import inputDetails from './inputDetails.ts';
-
+import checkExamStatus from "./checkExamStatus.ts"
 const router  = express.Router();
 
 router.use("/input-details", inputDetails);
+router.use("/check-exam-status", checkExamStatus);
 
 router.use(authMiddleware);
 
