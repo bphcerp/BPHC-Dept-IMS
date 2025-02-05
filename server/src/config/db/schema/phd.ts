b8e1d2d93955ce7f8fdf7dc0ce81ee64cd537e55
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2025-02-05 12:47:59 +0530

    Create applications table and update PhD schema

---
+++ server/src/config/db/schema/phd.ts
@@ -0,0 +1,39 @@
+import { pgTable, text, serial } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
+import { phd } from "./admin.ts";
+
+export const applications = pgTable("applications", {
+  application_id: serial("application_id").primaryKey(),
+  email: text("email")
+    .notNull()
+    .references(() => phd.email, { onDelete: "cascade" }),
+  file_id_1: text("file_id_1")
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+  file_id_2: text("file_id_2")
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+  file_id_3: text("file_id_3")
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+  file_id_4: text("file_id_4")
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+  file_id_5: text("file_id_5")
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+});
+
+export const applicationsRelations = relations(applications, ({ one }) => ({
+  phdUser: one(phd, {
+    fields: [applications.email],
+    references: [phd.email],
+    relationName: "phd",
+  }),
+}));
