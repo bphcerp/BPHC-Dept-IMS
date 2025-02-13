5eb942534f2a7ebeb9bfc5e6c160bed1d4024c53
achaldps@gmail.com <achaldps@gmail.com>
2025-02-13 19:19:07 +0530

    feat(relations): Update files relations to support many-to-one cardinality

--- server/src/config/db/schema/formRelations.ts
+++ server/src/config/db/schema/formRelations.ts
@@ -1,7 +1,12 @@
 import { relations } from "drizzle-orm";
-import { textFields, fileFields, textFieldStatus, fileFieldStatus,files } from "./form.ts";
+import {
+    textFields,
+    fileFields,
+    textFieldStatus,
+    fileFieldStatus,
+    files,
+} from "./form.ts";
 import { users } from "./admin.ts";
-
 
 export const usersRelations = relations(users, ({ many, one }) => ({
     textFieldStatus: one(textFieldStatus, {
@@ -17,50 +22,64 @@ export const usersRelations = relations(users, ({ many, one }) => ({
     files: many(files, {
         relationName: "files",
     }),
-}))
+}));
 
 export const textFieldsRelations = relations(textFields, ({ many }) => ({
     textFields: many(textFieldStatus, {
         relationName: "textFields",
     }),
-}))
+}));
 
 export const fileFieldsRelations = relations(fileFields, ({ many }) => ({
-    fileFields: many(fileFieldStatus, {
+    fileFieldStatus: many(fileFieldStatus, {
         relationName: "fileFields",
     }),
-}))
-
-export const textFieldStatusRelations = relations(textFieldStatus, ({ one }) => ({
-    textFieldStatus: one(users, {
-        fields: [textFieldStatus.userEmail],
-        references: [users.email],
-        relationName: "textFieldStatus",
+    file: many(files, {
+        relationName: "fileField",
     }),
-    textFields: one(textFields, {
-        fields: [textFieldStatus.textField],
-        references: [textFields.id],
-        relationName: "textFields",
-    })
 }));
 
-export const fileFieldStatusRelations = relations(fileFieldStatus, ({ one }) => ({
-    fileFieldStatus: one(users, {
-        fields: [fileFieldStatus.userEmail],
-        references: [users.email],
-        relationName: "fileFieldStatus",
-    }),
-    fileFields: one(fileFields, {
-        fields: [fileFieldStatus.fileField],
-        references: [fileFields.id],
-        relationName: "fileFields",
+export const textFieldStatusRelations = relations(
+    textFieldStatus,
+    ({ one }) => ({
+        textFieldStatus: one(users, {
+            fields: [textFieldStatus.userEmail],
+            references: [users.email],
+            relationName: "textFieldStatus",
+        }),
+        textFields: one(textFields, {
+            fields: [textFieldStatus.textField],
+            references: [textFields.id],
+            relationName: "textFields",
+        }),
     })
-}));
+);
+
+export const fileFieldStatusRelations = relations(
+    fileFieldStatus,
+    ({ one }) => ({
+        fileFieldStatus: one(users, {
+            fields: [fileFieldStatus.userEmail],
+            references: [users.email],
+            relationName: "fileFieldStatus",
+        }),
+        fileFields: one(fileFields, {
+            fields: [fileFieldStatus.fileField],
+            references: [fileFields.id],
+            relationName: "fileFields",
+        }),
+    })
+);
 
 export const fileRelations = relations(files, ({ one }) => ({
     files: one(users, {
         fields: [files.uploaded_by],
         references: [users.email],
         relationName: "files",
-    })
-}))
+    }),
+    fileField: one(fileFields, {
+        fields: [files.id],
+        references: [fileFields.file],
+        relationName: "fileField",
+    }),
+}));
