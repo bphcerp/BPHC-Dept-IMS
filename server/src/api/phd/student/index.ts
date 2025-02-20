6a9828f58f80846909e14a57323738c1d30e9248
Sanchay <sanchaynts9422@gmail.com>
2025-02-20 15:28:47 +0530

    refractor(phd): coursework form and deadline endpoint cleanup

--- server/src/api/phd/student/index.ts
+++ server/src/api/phd/student/index.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import { authMiddleware } from '@/middleware/auth.ts';
 import inputDetails from './inputDetails.ts';
 import checkExamStatus from "./checkExamStatus.ts"
-import getQualifyingExam from './getQualifyingExam.ts';
+import getQualifyingExamDeadLine from './getQualifyingExamDeadLine.ts';
 const router  = express.Router();
 
 router.use("/inputDetails", inputDetails);
 router.use("/checkExamStatus", checkExamStatus);
-router.use("/getQualifyingExam", getQualifyingExam);
+router.use("/getQualifyingExamDeadLine", getQualifyingExamDeadLine);
 
 router.use(authMiddleware);
 
