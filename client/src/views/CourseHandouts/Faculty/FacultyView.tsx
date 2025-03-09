d9469042080ada8d7c35271fd2c7bb15850d4a2c
Pegasus <100660420+Pegasus47@users.noreply.github.com>
2025-03-09 12:37:40 +0530

    Added file upload and made componenets for form

--- client/src/views/CourseHandouts/Faculty/FacultyView.tsx
+++ client/src/views/CourseHandouts/Faculty/FacultyView.tsx
@@ -1,20 +1,12 @@
-"use client";
-
-import { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useRef, useState } from "react";
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoadingSpinner } from "@/components/ui/spinner";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import { CustomFormField } from "@/components/coursehandouts/CustomFormField";
+import FileUploader from "@/components/coursehandouts/FileUploader";
 
 const schema = z.object({
   courseCode: z.string().nonempty("Course Code is required"),
@@ -28,10 +20,22 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
+
+
+const fieldConfigs = [
+  {name: "courseCode",label: "Course Code",placeholder: "e.g. CS101",type: "text",},
+  {name: "courseName",label: "Course Name",placeholder: "e.g. Computer Programming",type: "text",},
+  {name: "openBookPercentage",label: "Open Book Percentage",placeholder: "e.g. 30",type: "text",},
+  {name: "closedBookPercentage",label: "Closed Book Percentage",placeholder: "e.g. 70",type: "text",},
+  {name: "midsemesterWeightage",label: "Midsemester Weightage (in %)",placeholder: "e.g. 30",type: "text",},
+  {name: "comprehensiveWeightage",label: "Comprehensive Weightage (in %)",placeholder: "e.g. 70",type: "text",},
+  {name: "approximateStrength",label: "Approximate Course Strength",placeholder: "e.g. 120",type: "number",},
+];
+
 export default function CourseHandouts() {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const form = useForm<FormData>({
+  const methods = useForm<FormData>({
     resolver: zodResolver(schema),
     defaultValues: {
       courseCode: "",
@@ -60,144 +64,42 @@ export default function CourseHandouts() {
       <h1 className="text-2xl font-bold mb-8">
         HANDOUT APPROVAL - 2nd SEMESTER, YEAR 2024 - 2025
       </h1>
-      <Form {...form}>
-        <form
-          onSubmit={(e) => {
-            void form.handleSubmit(onSubmit)(e);
-          }}
-          className="space-y-8"
-        >
-          <div className="grid grid-cols-2 gap-4">
-            <FormField
-              control={form.control}
-              name="courseCode"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">Course Code</FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. CS101" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="courseName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">Course Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. Computer Programming" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
-            <FormField
-              control={form.control}
-              name="openBookPercentage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">
-                    Open Book Percentage
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. 30" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="closedBookPercentage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">
-                    Closed Book Percentage
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. 70" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
-            <FormField
-              control={form.control}
-              name="midsemesterWeightage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">
-                    Midsemester Weightage (in %)
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. 30" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="comprehensiveWeightage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">
-                    Comprehensive Weightage (in %)
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="e.g. 70" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="approximateStrength"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900">
-                    Approximate Course Strength
-                  </FormLabel>
-                  <FormControl>
-                    <Input type="number" placeholder="e.g. 120" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="flex flex-row justify-between mt-4 pt-8">
-            <div className="flex flex-col mb-6">
-              <Button variant="outline">Select Handout Docx</Button>
-              <p className="text-sm text-gray-500 mt-1 ml-2">
-                file.docx/file.pdf
-              </p>
+      <FormProvider {...methods}>
+        <Form {...methods}>
+          <form
+            onSubmit={(e) => {
+              void methods.handleSubmit(onSubmit)(e);
+            }}
+            className="space-y-8"
+          >
+            <div className="grid grid-cols-2 gap-4">
+              {fieldConfigs.map((config) => (
+                <CustomFormField key={config.name} {...config} />
+              ))}
             </div>
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              className="px-6 py-2 w-full sm:w-auto"
-            >
-              {isSubmitting ? (
-                <LoadingSpinner className="h-5 w-5" />
-              ) : (
-                "SUBMIT FOR VERIFICATION"
-              )}
-            </Button>
-          </div>
-        </form>
-      </Form>
+
+            <div className="flex flex-row justify-between mt-4 pt-8">
+              <div>
+                <FileUploader />
+                <p className="text-sm text-gray-500 mt-1 ml-2">
+                  file.docx/file.pdf
+                </p>
+              </div>
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                className="px-6 py-2 w-full sm:w-auto"
+              >
+                {isSubmitting ? (
+                  <LoadingSpinner className="h-5 w-5" />
+                ) : (
+                  "SUBMIT FOR VERIFICATION"
+                )}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </FormProvider>
     </div>
   );
 }
