96729f0162c704d06af096f9b5aeabc7f4510c72
soumitradev <soumitra.shewale.dev@gmail.com>
2025-03-13 20:47:17 +0530

    feat: implement viewApplicationDetails endpoint

--- server/src/api/conference/index.ts
+++ server/src/api/conference/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import createApplicationRouter from "./createApplication.ts";
+import viewApplicationDetailsRouter from "./viewApplicationDetails.ts";
 
 const router = express.Router();
 
 router.use("/createApplication", createApplicationRouter);
+router.use("/details", viewApplicationDetailsRouter);
 
 export default router;
