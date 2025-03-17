25a2324ce52791cacab8d9876a2d169a2c718b8a
soumitradev <soumitra.shewale.dev@gmail.com>
2025-03-17 12:39:29 +0530

    fix: fix bug in permissions map middleware that failed due to query params

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,6 +6,8 @@ import { HttpError, HttpCode } from "@/config/errors.ts";
 import { authUtils, permissions } from "lib";
 const permissionsMap: Record<string, string> = permissions;
 
+const dequerify = (x: string) => x.split("?")[0];
+
 /**
  * Middleware to check if the user has access to a specific operation.
  *
@@ -25,7 +27,9 @@ export function checkAccess(requiredOperation?: string) {
         }
         if (!requiredOperation)
             requiredOperation =
-                permissionsMap[PROD ? req.baseUrl : req.baseUrl.slice(4)];
+                permissionsMap[
+                    dequerify(PROD ? req.baseUrl : req.baseUrl.slice(4))
+                ];
         if (!requiredOperation)
             return next(
                 new HttpError(
