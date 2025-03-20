48f4075ae1d1fa3486dc0155cacd325ef023d835
kishan1735 <gswwarriorsnba@gmail.com>
2025-03-20 22:47:29 +0530

    feat: add assignReviewerAndIC endpoint

--- server/src/api/handout/index.ts
+++ server/src/api/handout/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import assignICandReviewer from "./assignICandReviewer.ts";
 
 const router = express.Router();
+
+router.use("/assignICandReviwer", assignICandReviewer);
 
 export default router;
