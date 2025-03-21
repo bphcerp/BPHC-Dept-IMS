f374dd13cc9eb63d8f74b0c996556b9d80a6d4bf
achaldps@gmail.com <achaldps@gmail.com>
2025-03-21 16:04:12 +0530

    fix: null problem in reviewer name field

--- server/src/api/handout/getAllHandoutsFaculty.ts
+++ server/src/api/handout/getAllHandoutsFaculty.ts
@@ -10,12 +10,17 @@ router.get(
     checkAccess("faculty-get-all-handouts"),
     asyncHandler(async (req, res, _next) => {
         assert(req.user);
-        
+
         const handouts = (
             await db.query.courseHandoutRequests.findMany({
-                where: (handout, { eq }) => eq(handout.icEmail, req.user?.email!),
+                where: (handout, { eq }) =>
+                    eq(handout.icEmail, req.user?.email!),
                 with: {
-                    reviewer: true,
+                    reviewer: {
+                        with: {
+                            faculty: true,
+                        },
+                    },
                 },
             })
         ).map((handout) => {
@@ -23,7 +28,7 @@ router.get(
                 id: handout.id,
                 courseName: handout.courseName,
                 courseCode: handout.courseCode,
-                reviewerName: handout.reviewer?.name || null
+                reviewerName: handout.reviewer?.faculty.name,
             };
         });
 
