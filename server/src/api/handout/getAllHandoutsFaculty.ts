d424652b52ac523e6ce23ba9f6cac2610e4b03a7
achaldps@gmail.com <achaldps@gmail.com>
2025-03-21 15:45:33 +0530

    fix: fetch only course code, course name and reviewer name from each course

--- server/src/api/handout/getAllHandoutsFaculty.ts
+++ server/src/api/handout/getAllHandoutsFaculty.ts
@@ -1,9 +1,7 @@
 import db from "@/config/db/index.ts";
-import { courseHandoutRequests } from "@/config/db/schema/handout.ts";
 import { checkAccess } from "@/middleware/auth.ts";
 import { asyncHandler } from "@/middleware/routeHandler.ts";
 import express from "express";
-import { eq } from "drizzle-orm";
 import assert from "assert";
 const router = express.Router();
 
@@ -12,13 +10,25 @@ router.get(
     checkAccess("faculty-get-all-handouts"),
     asyncHandler(async (req, res, _next) => {
         assert(req.user);
-        const courses = await db
-            .select()
-            .from(courseHandoutRequests)
-            .where(eq(courseHandoutRequests.icEmail, req.user.email));
+        
+        const handouts = (
+            await db.query.courseHandoutRequests.findMany({
+                where: (handout, { eq }) => eq(handout.icEmail, req.user?.email!),
+                with: {
+                    reviewer: true,
+                },
+            })
+        ).map((handout) => {
+            return {
+                courseName: handout.courseName,
+                courseCode: handout.courseCode,
+                reviewerName: handout.reviewer?.name || null
+            };
+        });
+
         res.status(200).json({
             success: true,
-            data: courses,
+            data: handouts,
         });
     })
 );
