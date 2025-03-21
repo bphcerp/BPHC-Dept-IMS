d5498692296a6d391228242b49289a0d5c7c9bf2
kishan1735 <gswwarriorsnba@gmail.com>
2025-03-21 19:11:19 +0530

    feat: add submit handout endpoint

--- server/src/api/handout/index.ts
+++ server/src/api/handout/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import assignICandReviewer from "./assignICandReviewer.ts";
 import getAllPendingHandoutsDCA from "./getAllPendingHandoutsDCA.ts";
+import submitHandout from "./submitHandout.ts";
 
 const router = express.Router();
 
 router.use("/assignICandReviewer", assignICandReviewer);
 router.use("/getAllPendingHandoutsDCA", getAllPendingHandoutsDCA);
+router.use("/submitHandout", submitHandout);
 
 export default router;
