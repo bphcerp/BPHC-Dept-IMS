4c3e53aa66e4370876e273e02adcee3979b47944
Sanchay <sanchaynts9422@gmail.com>
2025-03-21 18:12:40 +0530

    refractor: deleted unnecessay routes

--- server/src/api/phd/drcMember/index.ts
+++ server/src/api/phd/drcMember/index.ts
@@ -1,12 +1,6 @@
 import express from "express";
-import getPhDRecords from "./getPhD.ts";
 import updateExamDates from "./updateExamDates.ts";
-import getFacultyDetails from "./getFacultyDetails.ts"
-import assignSupervisor from "./assignSupervisor.ts"
-import assignNotionalSupervisor from "./assignNotionalSupervisor.ts"
-import getQualifyingExamForm from "./getQualifyingExamForm.ts"
 import generateCourseworkForm from "./generateCourseworkForm.ts"
-import updateQePassFailStatus from "./updateQePassFailStatus.ts";
 import getPhdToGenerateQualifyingExamForm from "./getPhdToGenerateQualifyingExamForm.ts";
 import getPhdThatPassedRecently from "./getPhdThatPassedRecently.ts";
 import updatePassingDatesOfPhd from "./updatePassingDatesOfPhd.ts";
@@ -16,11 +10,8 @@ import getSuggestedDacMember from "./getSuggestedDacMember.ts";
 import suggestTwoBestDacMember from "./suggestTwoBestDacMember.ts";
 import updateFinalDac from "./updateFinalDac.ts";
 import updateQualifyingExamResultsOfAllStudents from "./updateQualifyingExamResultsOfAllStudents.ts";
-import getAllQualifyingExam from "./getAllQualifyingExam.ts";
 import getAllQualifyingExamForTheSem from "./getAllQualifyingExamForTheSem.ts";
 import getAllSem from "./getAllSem.ts";
-import getCurrentActiveQualifyingExam from "./getCurrentActiveQualifyingExam.ts";
-import getSpecificSem from "./getSpecificSem.ts";
 import updateQualifyingExamDeadline from "./updateQualifyingExamDeadline.ts";
 import updateSemesterDates from "./updateSemesterDates.ts";
 import getCurrentSemester from "./getCurrentSemester.ts";
@@ -28,15 +19,10 @@ import getCurrentSemester from "./getCurrentSemester.ts";
 
 const router = express.Router();
 
-router.use("/getPhD", getPhDRecords);
+
 router.use("/updateExamDates", updateExamDates);
-router.use('/getFacultyDetails', getFacultyDetails);
-router.use('/assignSupervisor', assignSupervisor);
-router.use('/assignNotionalSupervisor', assignNotionalSupervisor);
-router.use('/getQualifyingExamForm', getQualifyingExamForm);
 router.use('/generateCourseworkForm', generateCourseworkForm);
 router.use('/updateQualifyingExamDeadline', updateQualifyingExamDeadline);
-router.use('/updateQePassFailStatus', updateQePassFailStatus);
 router.use('/generateCourseworkForm', generateCourseworkForm);
 router.use('/getPhdToGenerateQualifyingExamForm', getPhdToGenerateQualifyingExamForm);
 router.use('/updatePassingDatesOfPhd', updatePassingDatesOfPhd);
@@ -47,10 +33,7 @@ router.use('/getSuggestedDacMember', getSuggestedDacMember);
 router.use('/updateFinalDac', updateFinalDac);
 router.use('/suggestTwoBestDacMember', suggestTwoBestDacMember);
 router.use('/updateQualifyingExamResultsOfAllStudents', updateQualifyingExamResultsOfAllStudents);
-router.use('/getAllQualifyingExam', getAllQualifyingExam);
 router.use('/updateSemesterDates', updateSemesterDates);
-router.use('/getSpecificSem', getSpecificSem);
-router.use('/getCurrentActiveQualifyingExam', getCurrentActiveQualifyingExam);
 router.use('/getAllSem', getAllSem);
 router.use('/getAllQualifyingExamForTheSem', getAllQualifyingExamForTheSem);
 router.use('/getCurrentSemester', getCurrentSemester);
