bd632facde5b670ff01e0a5994e382c7b60229c8
Sanchay <sanchaynts9422@gmail.com>
2025-03-26 00:24:28 +0530

    refractor: removed unnecessay db schemas

--- server/src/config/db/schema/phd.ts
+++ server/src/config/db/schema/phd.ts
@@ -2,33 +2,6 @@ import { pgTable, text, serial, timestamp, integer ,  uuid} from "drizzle-orm/pg
 import { sql } from "drizzle-orm";
 import { phd } from "./admin.ts";
 
-export const phdApplications = pgTable("phd_applications", {
-    applicationId: serial("application_id").primaryKey(),
-    email: text("email")
-        .notNull()
-        .references(() => phd.email, { onDelete: "cascade" }),
-    fileId1: text("file_id_1")
-        .array()
-        .notNull()
-        .default(sql`'{}'::text[]`),
-    fileId2: text("file_id_2")
-        .array()
-        .notNull()
-        .default(sql`'{}'::text[]`),
-    fileId3: text("file_id_3")
-        .array()
-        .notNull()
-        .default(sql`'{}'::text[]`),
-    fileId4: text("file_id_4")
-        .array()
-        .notNull()
-        .default(sql`'{}'::text[]`),
-    fileId5: text("file_id_5")
-        .array()
-        .notNull()
-        .default(sql`'{}'::text[]`),
-});
-
 export const phdCourses = pgTable("phd_courses", {
     id: serial("id").primaryKey(),
     studentEmail: text("student_email")
@@ -46,21 +19,6 @@ export const phdCourses = pgTable("phd_courses", {
     courseIds: text("course_ids")
         .array()
         .default(sql`'{}'::text[]`),
-});
-
-export const phdConfig = pgTable("phd_config", {
-    key: text("key").notNull().unique(),
-    value: timestamp("value").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-});
-
-export const phdDocuments = pgTable("phdDocuments", {
-    id: uuid("id").defaultRandom().primaryKey(),
-    email: text("email").notNull(),
-    fileUrl: text("fileUrl").notNull(),
-    formName: text("formName").notNull(),
-    applicationType: text("applicationType").notNull(),
-    uploadedAt: timestamp("uploadedAt").defaultNow().notNull(),
 });
 
 export const phdSemesters = pgTable("phd_semesters", {
--- server/src/config/db/schema/phdRelations.ts
+++ server/src/config/db/schema/phdRelations.ts
@@ -1,14 +1,6 @@
 import { relations } from "drizzle-orm";
-import { phdApplications, phdCourses } from "./phd.ts";
+import {  phdCourses } from "./phd.ts";
 import { phd } from "./admin.ts";
-
-export const phdApplicationsRelations = relations(phdApplications, ({ one }) => ({
-    phdUser: one(phd, {
-        fields: [phdApplications.email],
-        references: [phd.email],
-        relationName: "phd",
-    }),
-}));
 
 export const phdCoursesRelations = relations(phdCourses, ({ one }) => ({
     student: one(phd, {
