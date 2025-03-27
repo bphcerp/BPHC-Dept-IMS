8def2bff73fa7c0d9aebd64718349b064e1a22fa
Shiv Patil <krymzin0311@gmail.com>
2025-03-27 16:52:45 +0530

    refactor(server): patents

--- server/src/api/patents/index.ts
+++ server/src/api/patents/index.ts
@@ -1,28 +1,8 @@
 import express from "express";
-import cors from "cors";
-import multer from "multer";
-import addNewPatent from "./addNewPatent";
-import addNewPatentManually from "./addNewPatentManually";
-import getAllPatents from "./getAllPatents";
-import getPatentsByInventorName from "./getPatentsByInventorName";
-import insertPatents from "./insertPatents";
-import saveInventorEmail from "./saveInventorEmail";
-import updateInventorEmail from "./updateInventorEmail";
+import addNewPatent from "./addNewPatents.ts";
 
-const app = express();
-const upload = multer(); // Multer instance for file handling
-
-app.use(express.json());
-app.use(cors());
 const router = express.Router();
 
-
 router.use("/addNewPatent", addNewPatent);
-router.use("/addNewPatentManually", addNewPatentManually);
-router.use("/getAllPatents", getAllPatents);
-router.use("/getPatentsByInventorName", getPatentsByInventorName);
-router.use("/insertPatents", insertPatents);
-router.use("/saveInventorEmail", saveInventorEmail);
-router.use("/updateInventorEmail", updateInventorEmail);
 
 export default router;
