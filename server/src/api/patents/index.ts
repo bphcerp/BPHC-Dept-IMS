2cb0c1cf5f2f513bdef71efed8220a5a40dd7e22
Abhinav Sai Yekkali <115100076+yekkaliabhinav@users.noreply.github.com>
2025-03-27 22:32:26 +0530

    Update index.ts

--- server/src/api/patents/index.ts
+++ server/src/api/patents/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import addNewPatent from "./addNewPatents.ts";
-
+import getAllPatents from "./getAllPatents.ts";
 const router = express.Router();
 
 router.use("/addNewPatent", addNewPatent);
+router.use("/getAllPatents",getAllPatents);
 
 export default router;
