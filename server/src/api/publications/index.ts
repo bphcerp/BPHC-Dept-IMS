dd7bcd331d69a5169913ec9b017624ed7731d0a7
Samir Khairati <samirk10101@gmail.com>
2025-04-09 02:05:37 +0530

    feat(server): added endpoint to get authorId of user

--- server/src/api/publications/index.ts
+++ server/src/api/publications/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 import userRouter from "./user.ts";
+import idRouter from "./id.ts";
 
 router.use("/user", userRouter);
+router.use("/id", idRouter);
 
 export default router;
