eebd3658d6f0294921fc7e05699e8d98c2b5a66c
Samir Khairati <samirk10101@gmail.com>
2025-04-09 01:37:30 +0530

    feat(lib): added zod schemas for publications

---
+++ lib/src/schemas/Publications.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+export const PublicationSchema = z.object({
+    citationId: z.string(),
+    title: z.string(),
+    type: z.string().nullable(),
+    journal: z.string().nullable(),
+    volume: z.string().nullable(),
+    issue: z.string().nullable(),
+    year: z.string().nullable(),
+    link: z.string().nullable(),
+    citations: z.string().nullable(),
+    authorNames: z.string().nullable(),
+});
+
+export const CoAuthorSchema = z.object({
+    authorId: z.string(),
+    authorName: z.string().nullable(),
+});
+
+export const PublicationWithCoAuthorsSchema = PublicationSchema.extend({
+    coAuthors: z.array(CoAuthorSchema),
+});
+
+export const PublicationRowSchema = z.object({
+    publication: PublicationSchema,
+    authorId: z.string(),
+    authorName: z.string().nullable(),
+});
+
+export const publicationQuerySchema = z.object({
+    authorId: z.string(),
+});
+
+export const publicationResponseSchema = z.object({
+    publications: z.array(PublicationWithCoAuthorsSchema),
+});
+
+export type Publication = z.infer<typeof PublicationSchema>;
+export type CoAuthor = z.infer<typeof CoAuthorSchema>;
+export type PublicationWithCoAuthors = z.infer<
+    typeof PublicationWithCoAuthorsSchema
+>;
+export type PublicationRow = z.infer<typeof PublicationRowSchema>;
+export type PublicationQuery = z.infer<typeof publicationQuerySchema>;
+export type PublicationResponse = z.infer<typeof publicationResponseSchema>;
