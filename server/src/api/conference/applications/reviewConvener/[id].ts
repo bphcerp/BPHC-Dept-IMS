ac023f738967bcbfa296bc58a3a1b4a81da157b2
soumitradev <soumitra.shewale.dev@gmail.com>
2025-04-15 05:29:25 +0530

    fix: modify conference application review endpoints to fit new flow

--- server/src/api/conference/applications/reviewConvener/[id].ts
+++ server/src/api/conference/applications/reviewConvener/[id].ts
@@ -1,85 +1,85 @@
-// import { HttpCode, HttpError } from "@/config/errors.ts";
-// import { asyncHandler } from "@/middleware/routeHandler.ts";
-// import express from "express";
-// import { authUtils, conferenceSchemas } from "lib";
-// import { getApplicationById } from "@/lib/conference/index.ts";
-// import db from "@/config/db/index.ts";
-// import { applications } from "@/config/db/schema/form.ts";
-// import { eq } from "drizzle-orm";
-// import { conferenceApprovalApplications } from "@/config/db/schema/conference.ts";
+import { HttpCode, HttpError } from "@/config/errors.ts";
+import { asyncHandler } from "@/middleware/routeHandler.ts";
+import express from "express";
+import { authUtils, conferenceSchemas } from "lib";
+import { getApplicationById } from "@/lib/conference/index.ts";
+import db from "@/config/db/index.ts";
+import {
+    conferenceApprovalApplications,
+    conferenceMemberReviews,
+} from "@/config/db/schema/conference.ts";
+import { eq } from "drizzle-orm";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post(
-//     "/:id",
-//     asyncHandler(async (req, res, next) => {
-//         const id = parseInt(req.params.id);
-//         if (isNaN(id) || id <= 0)
-//             return next(new HttpError(HttpCode.BAD_REQUEST, "Invalid id"));
+router.post(
+    "/:id",
+    asyncHandler(async (req, res, next) => {
+        const id = parseInt(req.params.id);
+        if (isNaN(id) || id <= 0)
+            return next(new HttpError(HttpCode.BAD_REQUEST, "Invalid id"));
 
-//         const { status } = conferenceSchemas.reviewApplicationBodySchema.parse(
-//             req.body
-//         );
+        const { status, comments } =
+            conferenceSchemas.reviewApplicationBodySchema.parse(req.body);
 
-//         const isHoD = authUtils.checkAccess(
-//             "conference:application:review-application-hod",
-//             req.user!.permissions
-//         );
+        const isHoD = authUtils.checkAccess(
+            "conference:application:review-application-hod",
+            req.user!.permissions
+        );
 
-//         const isConvener = authUtils.checkAccess(
-//             "conference:application:review-application-convener",
-//             req.user!.permissions
-//         );
+        if (isHoD)
+            return next(
+                new HttpError(
+                    HttpCode.FORBIDDEN,
+                    "You are not allowed to review this application yet"
+                )
+            );
 
-//         if (!isHoD && !isConvener)
-//             return next(
-//                 new HttpError(
-//                     HttpCode.FORBIDDEN,
-//                     "You are not allowed to review this application"
-//                 )
-//             );
+        const application = await getApplicationById(id);
 
-//         const application = await getApplicationById(id);
+        if (!application)
+            return next(
+                new HttpError(HttpCode.NOT_FOUND, "Application not found")
+            );
 
-//         if (!application)
-//             return next(
-//                 new HttpError(HttpCode.NOT_FOUND, "Application not found")
-//             );
+        const applicationStateIndex = conferenceSchemas.states.indexOf(
+            application.state
+        );
+        if (applicationStateIndex !== 2)
+            return next(
+                new HttpError(
+                    HttpCode.BAD_REQUEST,
+                    applicationStateIndex < 2
+                        ? "Application is not ready to be reviewed yet"
+                        : "Application is already reviewed by DRC Convener"
+                )
+            );
 
-//         if (application.status !== "pending")
-//             return next(
-//                 new HttpError(
-//                     HttpCode.BAD_REQUEST,
-//                     "Application is already reviewed"
-//                 )
-//             );
+        await db.transaction(async (tx) => {
+            await tx
+                .update(conferenceApprovalApplications)
+                .set({
+                    state: conferenceSchemas.states[
+                        applicationStateIndex + (status ? 1 : -2)
+                    ],
+                })
+                .where(eq(conferenceApprovalApplications.id, id));
 
-//         await db.transaction(async (tx) => {
-//             await tx
-//                 .update(conferenceApprovalApplications)
-//                 .set({
-//                     state:
-//                         isHoD && status
-//                             ? conferenceSchemas.states[3]
-//                             : isHoD
-//                               ? conferenceSchemas.states[2]
-//                               : conferenceSchemas.states[1],
-//                 })
-//                 .where(
-//                     eq(
-//                         conferenceApprovalApplications.applicationId,
-//                         application.id
-//                     )
-//                 );
-//             if (isHoD || !status)
-//                 await tx
-//                     .update(applications)
-//                     .set({ status: status ? "approved" : "rejected" })
-//                     .where(eq(applications.id, application.id));
-//         });
+            tx.delete(conferenceMemberReviews).where(
+                eq(conferenceMemberReviews.applicationId, id)
+            );
 
-//         res.status(200).send();
-//     })
-// );
+            tx.insert(conferenceMemberReviews).values([
+                {
+                    applicationId: application.id,
+                    reviewerEmail: req.user!.email,
+                    status: status,
+                    comments: comments,
+                },
+            ]);
+        });
+        res.status(200).send();
+    })
+);
 
-// export default router;
+export default router;
--- server/src/api/conference/applications/reviewMember/[id].ts
+++ server/src/api/conference/applications/reviewMember/[id].ts
@@ -1,85 +1,103 @@
-// import { HttpCode, HttpError } from "@/config/errors.ts";
-// import { asyncHandler } from "@/middleware/routeHandler.ts";
-// import express from "express";
-// import { authUtils, conferenceSchemas } from "lib";
-// import { getApplicationById } from "@/lib/conference/index.ts";
-// import db from "@/config/db/index.ts";
-// import { applications } from "@/config/db/schema/form.ts";
-// import { eq } from "drizzle-orm";
-// import { conferenceApprovalApplications } from "@/config/db/schema/conference.ts";
+import { HttpCode, HttpError } from "@/config/errors.ts";
+import { asyncHandler } from "@/middleware/routeHandler.ts";
+import express from "express";
+import { authUtils, conferenceSchemas, permissions } from "lib";
+import { getApplicationById } from "@/lib/conference/index.ts";
+import db from "@/config/db/index.ts";
+import {
+    conferenceApprovalApplications,
+    conferenceMemberReviews,
+} from "@/config/db/schema/conference.ts";
+import { eq } from "drizzle-orm";
+import { getUsersWithPermission } from "@/lib/common/index.ts";
+import { checkAccess, dequerify } from "@/middleware/auth.ts";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post(
-//     "/:id",
-//     asyncHandler(async (req, res, next) => {
-//         const id = parseInt(req.params.id);
-//         if (isNaN(id) || id <= 0)
-//             return next(new HttpError(HttpCode.BAD_REQUEST, "Invalid id"));
+router.post(
+    "/:id",
+    checkAccess(),
+    asyncHandler(async (req, res, next) => {
+        const id = parseInt(req.params.id);
+        if (isNaN(id) || id <= 0)
+            return next(new HttpError(HttpCode.BAD_REQUEST, "Invalid id"));
 
-//         const { status } = conferenceSchemas.reviewApplicationBodySchema.parse(
-//             req.body
-//         );
+        const { status, comments } =
+            conferenceSchemas.reviewApplicationBodySchema.parse(req.body);
 
-//         const isHoD = authUtils.checkAccess(
-//             "conference:application:review-application-hod",
-//             req.user!.permissions
-//         );
+        const isHoD = authUtils.checkAccess(
+            "conference:application:review-application-hod",
+            req.user!.permissions
+        );
 
-//         const isConvener = authUtils.checkAccess(
-//             "conference:application:review-application-convener",
-//             req.user!.permissions
-//         );
+        const isConvener = authUtils.checkAccess(
+            "conference:application:review-application-convener",
+            req.user!.permissions
+        );
 
-//         if (!isHoD && !isConvener)
-//             return next(
-//                 new HttpError(
-//                     HttpCode.FORBIDDEN,
-//                     "You are not allowed to review this application"
-//                 )
-//             );
+        if (isHoD || isConvener)
+            return next(
+                new HttpError(
+                    HttpCode.FORBIDDEN,
+                    "You are not allowed to review this application yet"
+                )
+            );
 
-//         const application = await getApplicationById(id);
+        const application = await getApplicationById(id);
+        if (!application)
+            return next(
+                new HttpError(HttpCode.NOT_FOUND, "Application not found")
+            );
 
-//         if (!application)
-//             return next(
-//                 new HttpError(HttpCode.NOT_FOUND, "Application not found")
-//             );
+        const applicationStateIndex = conferenceSchemas.states.indexOf(
+            application.state
+        );
+        if (applicationStateIndex !== 1)
+            return next(
+                new HttpError(
+                    HttpCode.BAD_REQUEST,
+                    applicationStateIndex < 1
+                        ? "Application is not ready to be reviewed yet"
+                        : "Application is already reviewed by DRC Members"
+                )
+            );
 
-//         if (application.status !== "pending")
-//             return next(
-//                 new HttpError(
-//                     HttpCode.BAD_REQUEST,
-//                     "Application is already reviewed"
-//                 )
-//             );
+        const otherReviewers = new Set(
+            (
+                await db.query.conferenceMemberReviews.findMany({
+                    where: (r) => eq(r.applicationId, id),
+                })
+            ).map((u) => u.reviewerEmail)
+        );
+        otherReviewers.add(req.user!.email);
+        const allReviewers = new Set(
+            (
+                await getUsersWithPermission(
+                    permissions[dequerify(req.baseUrl)]
+                )
+            ).map((u) => u.email)
+        );
 
-//         await db.transaction(async (tx) => {
-//             await tx
-//                 .update(conferenceApprovalApplications)
-//                 .set({
-//                     state:
-//                         isHoD && status
-//                             ? conferenceSchemas.states[3]
-//                             : isHoD
-//                               ? conferenceSchemas.states[2]
-//                               : conferenceSchemas.states[1],
-//                 })
-//                 .where(
-//                     eq(
-//                         conferenceApprovalApplications.applicationId,
-//                         application.id
-//                     )
-//                 );
-//             if (isHoD || !status)
-//                 await tx
-//                     .update(applications)
-//                     .set({ status: status ? "approved" : "rejected" })
-//                     .where(eq(applications.id, application.id));
-//         });
+        await db.transaction(async (tx) => {
+            if (allReviewers.difference(otherReviewers).size === 0) {
+                // If all DRC Members have reviewed the application, move state to DRC member
+                await tx.update(conferenceApprovalApplications).set({
+                    state: conferenceSchemas.states[applicationStateIndex + 1],
+                });
+            }
 
-//         res.status(200).send();
-//     })
-// );
+            tx.insert(conferenceMemberReviews).values([
+                {
+                    applicationId: application.id,
+                    reviewerEmail: req.user!.email,
+                    status: status,
+                    comments: comments,
+                },
+            ]);
+        });
 
-// export default router;
+        res.status(200).send();
+    })
+);
+
+export default router;
