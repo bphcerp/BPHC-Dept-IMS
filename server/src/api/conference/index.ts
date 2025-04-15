4e4e6061749c118216bfb9314e06f977e257f63a
soumitradev <soumitra.shewale.dev@gmail.com>
2025-04-15 06:19:30 +0530

    refactor: rename old endpoints to fit new functions

--- server/src/api/conference/index.ts
+++ server/src/api/conference/index.ts
@@ -2,13 +2,13 @@ import express from "express";
 import createApplicationRouter from "./createApplication.ts";
 import getFlowRouter from "./getFlow.ts";
 import setFlowRouter from "./setFlow.ts";
-import editApplicationFieldRouter from "./editApplication.ts";
+import editApplicationRouter from "./editApplication.ts";
 import applicationsRouter from "./applications/index.ts";
 
 const router = express.Router();
 
 router.use("/createApplication", createApplicationRouter);
-router.use("/edit", editApplicationFieldRouter);
+router.use("/edit", editApplicationRouter);
 router.use("/applications", applicationsRouter);
 router.use("/getFlow", getFlowRouter);
 router.use("/setFlow", setFlowRouter);
