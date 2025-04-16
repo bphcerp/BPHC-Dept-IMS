8b7c2786a948e4fb513a8fb00964361a3ddcd321
Shreyash <shreyash1264@gmail.com>
2025-04-16 16:32:10 +0530

    refactor: modify schema to accomodate new changes

--- server/src/config/db/schema/qp.ts
+++ server/src/config/db/schema/qp.ts
@@ -10,55 +10,57 @@ import {
 import { fileFields } from "./form.ts";
 import { users } from "./admin.ts";
 import { jsonb } from "drizzle-orm/pg-core";
+import { qpSchemas } from "lib";
 
-export const qpStatusEnum = pgEnum("qp_status_enum", [
-    "pending",
-    "approved",
-    "rejected",
-]);
+export const qpStatusEnum = pgEnum(
+    "qp_status_enum",
+    qpSchemas.qpReviewStatuses
+);
+
+export const categoryEnum = pgEnum("category_enum", qpSchemas.categories);
 
 export const qpReviewRequests = pgTable("qp_review_requests", {
     id: serial("id").primaryKey(),
-    dcaMemberEmail: text("dca_member_email")
-    .notNull()
-    .references(() => users.email, { onDelete: "cascade" }), 
-    ficEmail: text("fic_email").references(() => users.email, {
+    icEmail: text("ic_email").references(() => users.email, {
         onDelete: "cascade",
     }),
-    faculty1Email: text("faculty_1_email").references(() => users.email, {
-        onDelete: "cascade",
-    }),
-    faculty2Email: text("faculty_2_email").references(() => users.email, {
+    reviewerEmail: text("reviewer_email").references(() => users.email, {
         onDelete: "cascade",
     }),
     courseName: text("course_name").notNull(),
     courseCode: text("course_code").notNull(),
+    previousSubmissionId: integer("previous_submission_id"),
+    midSemFilePath: integer("mid_sem_file_path").references(
+        () => fileFields.id,
+        {
+            onDelete: "set null",
+        }
+    ),
+    midSemSolFilePath: integer("mid_sem_sol_file_path").references(
+        () => fileFields.id,
+        {
+            onDelete: "set null",
+        }
+    ),
+    compreFilePath: integer("compre_file_path").references(
+        () => fileFields.id,
+        {
+            onDelete: "set null",
+        }
+    ),
+    compreSolFilePath: integer("compre_sol_file_path").references(
+        () => fileFields.id,
+        {
+            onDelete: "set null",
+        }
+    ),
+    review: jsonb("review"),
     ficDeadline: timestamp("fic_deadline", { withTimezone: true }),
     reviewDeadline: timestamp("review_deadline", { withTimezone: true }),
-    midSemFileId: integer("mid_sem_file_id").references(() => fileFields.id, {
-        onDelete: "set null",
-    }),
-    midSemSolFileId: integer("mid_sem_sol_file_id").references(
-        () => fileFields.id,
-        {
-            onDelete: "set null",
-        }
-    ),
-    compreFileId: integer("compre_file_id").references(() => fileFields.id, {
-        onDelete: "set null",
-    }),
-    compreSolFileId: integer("compre_sol_file_id").references(
-        () => fileFields.id,
-        {
-            onDelete: "set null",
-        }
-    ),
-    review1: jsonb("review_1"), 
-    review2: jsonb("review_2"),
-    reviewed: text("reviewed").notNull().default("review pending"), 
     documentsUploaded: boolean("documents_uploaded").notNull().default(false),
-    status: qpStatusEnum("status").notNull().default("pending"),
+    status: qpStatusEnum("status").notNull().default("notsubmitted"),
     createdAt: timestamp("created_at", { withTimezone: true })
         .notNull()
         .defaultNow(),
+    category: categoryEnum("category").notNull(),
 });
--- server/src/config/db/schema/qpRelations.ts
+++ server/src/config/db/schema/qpRelations.ts
@@ -3,45 +3,38 @@ import { qpReviewRequests } from "./qp.ts";
 import { users } from "./admin.ts";
 import { fileFields } from "./form.ts";
 
-export const qpReviewRequestsRelations = relations(qpReviewRequests, ({ one }) => ({
-    dcaMember: one(users, {
-        fields: [qpReviewRequests.dcaMemberEmail],
-        references: [users.email],
-        relationName: "qpDcaMember",
-    }),
-    fic: one(users, {
-        fields: [qpReviewRequests.ficEmail],
-        references: [users.email],
-        relationName: "qpFic",
-    }),
-    faculty1: one(users, {
-        fields: [qpReviewRequests.faculty1Email],
-        references: [users.email],
-        relationName: "qpFaculty1",
-    }),
-    faculty2: one(users, {
-        fields: [qpReviewRequests.faculty2Email],
-        references: [users.email],
-        relationName: "qpFaculty2",
-    }),
-    midSemFile: one(fileFields, {
-        fields: [qpReviewRequests.midSemFileId],
-        references: [fileFields.id],
-        relationName: "qpMidSemFile",
-    }),
-    midSemSolFile: one(fileFields, {
-        fields: [qpReviewRequests.midSemSolFileId],
-        references: [fileFields.id],
-        relationName: "qpMidSemSolFile",
-    }),
-    compreFile: one(fileFields, {
-        fields: [qpReviewRequests.compreFileId],
-        references: [fileFields.id],
-        relationName: "qpCompreFile",
-    }),
-    compreSolFile: one(fileFields, {
-        fields: [qpReviewRequests.compreSolFileId],
-        references: [fileFields.id],
-        relationName: "qpCompreSolFile",
-    }),
-}));
+export const qpReviewRequestsRelations = relations(
+    qpReviewRequests,
+    ({ one }) => ({
+        ic: one(users, {
+            fields: [qpReviewRequests.icEmail],
+            references: [users.email],
+            relationName: "ic",
+        }),
+        reviewer: one(users, {
+            fields: [qpReviewRequests.reviewerEmail],
+            references: [users.email],
+            relationName: "qpFaculty1",
+        }),
+        midSemFilePath: one(fileFields, {
+            fields: [qpReviewRequests.midSemFilePath],
+            references: [fileFields.id],
+            relationName: "qpMidSemFilePath",
+        }),
+        midSemSolFile: one(fileFields, {
+            fields: [qpReviewRequests.midSemSolFilePath],
+            references: [fileFields.id],
+            relationName: "qpMidSemSolFile",
+        }),
+        compreFile: one(fileFields, {
+            fields: [qpReviewRequests.compreFilePath],
+            references: [fileFields.id],
+            relationName: "qpCompreFile",
+        }),
+        compreSolFile: one(fileFields, {
+            fields: [qpReviewRequests.compreSolFilePath],
+            references: [fileFields.id],
+            relationName: "qpCompreSolFile",
+        }),
+    })
+);
