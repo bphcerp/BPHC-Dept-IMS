855a4f3bed04b2ac78fc0ca9a08048e88935e20a
Shiv Patil <krymzin0311@gmail.com>
2025-06-25 10:11:58 +0530

    chore(db): migrations

---
+++ server/drizzle/0031_lowly_squadron_supreme.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "course_handout_requests" ADD COLUMN "open_book" integer;--> statement-breakpoint
+ALTER TABLE "course_handout_requests" ADD COLUMN "midsem" integer;--> statement-breakpoint
+ALTER TABLE "course_handout_requests" ADD COLUMN "compre" integer;--> statement-breakpoint
+ALTER TABLE "course_handout_requests" ADD COLUMN "other_evals" integer;
