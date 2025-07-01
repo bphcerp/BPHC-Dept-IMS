88e1124b31cd45aa40c20c316ca2a50207bb9ce3
Shiv Patil <krymzin0311@gmail.com>
2025-07-01 09:28:14 +0530

    refactor: rename developers page > contributors page

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -9,7 +9,7 @@ import fileRouter from "./file/index.ts";
 import publicationsRouter from "./publications/index.ts";
 import inventoryRouter from "./inventory/index.ts";
 import profileRouter from "./profile/index.ts";
-import developersRouter from "./developers.ts";
+import contributorsRouter from "./contributors.ts";
 import todosRoute from "./todos.ts";
 import clearNotificationsRoute from "./clearNotifications.ts";
 import readNotificationsRoute from "./readNotifications.ts";
@@ -28,7 +28,7 @@ router.get("/hello", (_req, res) => {
     });
 });
 
-router.use("/developers", developersRouter)
+router.use("/contributors", contributorsRouter)
 
 router.get("/favicon.ico", (_req, res) => {
     const headers = new Headers({
