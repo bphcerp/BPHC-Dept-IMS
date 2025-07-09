7cd8e4a708d167a651167142335b03231dde89fc
Radian6405 <vardhan.6405@gmail.com>
2025-07-09 20:26:25 +0530

    feat: view routes for WILP projects

--- server/src/api/wilpProject/index.ts
+++ server/src/api/wilpProject/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import upload from "./upload.ts";
+import viewAll from "./view-all.ts";
+import viewSelected from "./view-selected.ts";
+import viewDetails from "./view-details.ts";
 
 const router = express.Router();
 
 router.use("/upload", upload);
+router.use("/view/all", viewAll);
+router.use("/view/selected", viewSelected);
+router.use("/view", viewDetails);
 
 export default router;
