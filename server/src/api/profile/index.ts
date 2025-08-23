b84a0393a00555043592c1bd3572ea4dd5d20931
Radian6405 <vardhan.6405@gmail.com>
2025-08-23 23:08:22 +0530

    feat: edit profile routes with input schemas

--- server/src/api/profile/index.ts
+++ server/src/api/profile/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import signatureRouter from "./signature.ts";
 import profileImageRouter from "./profile-image.ts";
+import editRouter from "./edit.ts";
 
 const router = express.Router();
 
 router.use("/signature", signatureRouter);
+router.use("/edit", editRouter);
 router.use("/profile-image", profileImageRouter);
 
 export default router;
