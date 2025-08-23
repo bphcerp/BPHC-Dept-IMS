76cedcbcd168243ca2cca4b17b3b2063984a78be
Radian6405 <vardhan.6405@gmail.com>
2025-08-23 23:14:24 +0530

    feat: get my profile details route

--- server/src/api/profile/index.ts
+++ server/src/api/profile/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import signatureRouter from "./signature.ts";
 import profileImageRouter from "./profile-image.ts";
 import editRouter from "./edit.ts";
+import myProfileRouter from "./myProfile.ts";
 
 const router = express.Router();
 
 router.use("/signature", signatureRouter);
 router.use("/edit", editRouter);
+router.use("/", myProfileRouter);
 router.use("/profile-image", profileImageRouter);
 
 export default router;
