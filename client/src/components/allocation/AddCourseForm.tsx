1f62b215206ca12bf5af57c0b46cafcfbe7124c8
obsi-git <sidbha468@gmail.com>
2025-08-29 12:58:29 +0530

    feat (allocation): Add button to add courses to the courses table
    Add a button to add courses in the Courses view of the Course Load
    Allocation section.
    Fix the form that pops up, which is now two columns
    wide and section values support 0.

---
+++ client/src/components/allocation/AddCourseForm.tsx
@@ -0,0 +1,150 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { courseSchema } from "../../../../lib/src/schemas/Allocation"
+import { NewCourse, Course } from "../../../../lib/src/types/allocation"
+
+
+interface AddCourseFormProps {
+  children: React.ReactNode;
+  onCourseAdded: (newCourse: Course) => void;
+}
+
+const AddCourseForm = ({children, onCourseAdded}: AddCourseFormProps) => {
+  const form = useForm<NewCourse>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: {
+      isCDC: false,
+    },
+  });
+ 
+  const onSubmit = async (values: NewCourse) => {
+    console.log("Form submitted with values:", values);
+
+    const newCourse: Course = {
+      ...values,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    
+    onCourseAdded(newCourse);
+    form.reset();
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-2xl">  
+        <DialogHeader>
+          <DialogTitle> Add a New Course </DialogTitle>
+          <DialogDescription> Fill in the details of the new course </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4"> 
+            <div className="flex space-x-4"> 
+              <div className="flex-1 space-y-4">
+                <FormField control={form.control} name="code" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Course Code </FormLabel>
+                    <FormControl> 
+                      <Input placeholder="e.g. CS F211" {...field}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+                 <FormField control={form.control} name="name" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Course Title </FormLabel>
+                    <FormControl> 
+                      <Input placeholder="e.g. Data Structures and Algorithms" {...field}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+                <FormField control={form.control} name="units" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Credits </FormLabel>
+                    <FormControl> 
+                      <Input type="number" placeholder="Enter the number of credits." {...field} onChange={event => field.onChange(+event.target.value)}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+
+                <FormField control={form.control} name="isCDC" render={({field}) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                    <FormControl>
+                      <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel> Is this a Core Disciplinary Course (CDC)? </FormLabel>
+                    </div>
+                  </FormItem>
+                )}
+                />
+              </div>
+
+              <div className="flex-1 space-y-4"> 
+                <FormField control={form.control} name="lectureSecCount" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Lecture Sections </FormLabel>
+                    <FormControl> 
+                      <Input type="number" placeholder="Enter the number of lecture sections." {...field} onChange={event => field.onChange(+event.target.value)}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+                <FormField control={form.control} name="tutSecCount" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Tutorial Sections </FormLabel>
+                    <FormControl> 
+                      <Input type="number" placeholder="Enter the number of tutorial sections." {...field} onChange={event => field.onChange(+event.target.value)}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+                <FormField control={form.control} name="practicalSecCount" render={({field}) => (
+                  <FormItem>
+                    <FormLabel> Practical Sections </FormLabel>
+                    <FormControl> 
+                      <Input type="number" placeholder="Enter the number of practical sections." {...field} onChange={event => field.onChange(+event.target.value)}/>
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+                />
+              </div>
+            </div>
+            <Button type="submit"> Add Course </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+  export { AddCourseForm };
+
--- client/src/views/Allocation/CourseLoadPage.tsx
+++ client/src/views/Allocation/CourseLoadPage.tsx
@@ -1,14 +1,108 @@
-import { CourseLoadTable } from "@/components/allocation/CourseLoadTable";
-// import { DataTable } from "@/components/shared/datatable/DataTable";
+import { DataTable } from "@/components/shared/datatable/DataTable";
+import { Course } from "../../../../lib/src/types/allocation.ts"
+import { ColumnDef } from "@tanstack/react-table"
+import { useEffect, useState } from "react"
+import { Button } from "@/components/ui/button"
+import { AddCourseForm } from "@/components/allocation/AddCourseForm"
+
+const columns: ColumnDef<Course>[] = [
+  {
+    accessorKey: "code",
+    header: "Course Code",
+  },
+  
+  {
+    accessorKey: "name",
+    header: "Course Title",
+  },
+
+  {
+    accessorKey: "units",
+    header: "Units",
+  },
+
+  {
+    accessorKey: "lectureSecCount",
+    header: "Lecture Sections",
+    cell: ({ row }) => {
+      const value = row.original.lectureSecCount;
+      return value === 0 ? "0" : value;
+    }
+  },
+
+  {
+    accessorKey: "tutSecCount",
+    header: "Tutorial Sections",
+    cell: ({ row }) => {
+      const value = row.original.tutSecCount;
+      return value === 0 ? "0" : value;
+    }
+
+  },
+
+  {
+    accessorKey: "practicalSecCount",
+    header: "Practical Sections",
+    cell: ({ row }) => {
+      const value = row.original.practicalSecCount;
+      return value === 0 ? "0" : value;
+    }
+
+  },
+
+  {
+    accessorKey: "isCDC",
+    header: "Is CDC?",
+    cell: ({ row }) => (row.original.isCDC) ? "Yes" : "No",
+  },
+];
 
 const CourseLoadPage = () => {
+  const[courses, setCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const mockCourses: Course[] = [
+        {
+          code: "CS F211",
+          name: "Data Structures and Algorithms",
+          lectureSecCount: 1,
+          tutSecCount: 2,
+          practicalSecCount: 2,
+          units: 4,
+          isCDC: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      ];
+      setCourses(mockCourses);
+    };
+    fetchCourses();
+  }, []);
+
+  const handleCourseAdded = (newCourse: Course) => {
+    setCourses((prevCourses) => [...prevCourses, newCourse]);
+  };
+
+  const addCourseButton = (
+    <AddCourseForm onCourseAdded={handleCourseAdded}>
+      <Button> Add Course </Button>
+    </AddCourseForm>
+  );
+
   return (
     <div className="p-4">
-      <h1 className="test-2xl font-bold mb-4"> Course Load Allocation </h1>
-      {/* <DataTable /> use this once we get the api setup */}
-      <CourseLoadTable />
+      <h1 className="text-2xl font-bold mb-4"> Course Load allocation </h1>
+      <DataTable 
+        columns={columns}
+        data={courses}
+        mainSearchColumn="name"
+        additionalButtons={addCourseButton}
+        />
     </div>
   );
 };
 
 export default CourseLoadPage;
+
+
