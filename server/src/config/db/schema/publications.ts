479f8e79711d7d5b34a2efd96737df61a92a45c6
hans772 <tobitkjoshy@gmail.com>
2025-09-01 08:56:23 +0530

    feat: created new table for researgence data

--- server/src/config/db/schema/publications.ts
+++ server/src/config/db/schema/publications.ts
@@ -1,4 +1,4 @@
-import { pgTable, primaryKey, text, boolean, pgEnum } from "drizzle-orm/pg-core";
+import { pgTable, primaryKey, text, boolean, pgEnum, integer, numeric, index } from "drizzle-orm/pg-core";
 import { publicationsSchemas } from "lib"
 
 export const monthEnum = pgEnum("month", publicationsSchemas.months); 
@@ -16,6 +16,39 @@ export const publicationsTable = pgTable("publications", {
     citationId: text("citation_id").primaryKey(),
     authorNames: text("author_names"),
 });
+
+export const researgencePublications = pgTable("researgence", {
+  pubId: integer("pub_id").primaryKey(),
+  authors: text("authors"),
+  homeAuthors: text("home_authors"),
+  homeAuthorDepartment: text("home_author_department"),
+  homeAuthorInstitute: text("home_author_institute"),
+  publicationTitle: text("publication_title"),
+  scs: integer("scs"),
+  wos: integer("wos"),
+  sci: text("sci"),
+  sourcePublication: text("source_publication"),
+  level: text("level"),
+  articleType: text("article_type"),
+  year: integer("year"),
+  month: text("month"),
+  homeAuthorLocation: text("home_author_location"),
+  volNo: text("vol_no"),
+  issNo: text("iss_no"),
+  bPage: text("b_page"),
+  ePage: text("e_page"),
+  snip: numeric("snip"),
+  sjr: numeric("sjr"),
+  impactFactor: numeric("if"),
+  citeScore: numeric("cite_score"),
+  qRankScs: text("q_rank_scs"),
+  qRankWos: text("q_rank_wos"),
+  pIssn: text("p_issn"),
+  eIssn: text("e_issn"),
+  pIsbn: text("p_isbn"),
+  eIsbn: text("e_isbn"),
+  link: text("link"),
+}, (table) => [index("title_idx").on(table.publicationTitle)]);
 
 export const authorPublicationsTable = pgTable(
     "author_publications",
