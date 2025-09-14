d20cd89bd9089b23bae4b9f40e9c4a88f5acc374
Radian6405 <vardhan.6405@gmail.com>
2025-09-15 00:04:11 +0530

    feat: end testing route

--- server/src/api/admin/testing/index.ts
+++ server/src/api/admin/testing/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import startRouter from "./start.ts";
+import endRouter from "./end.ts";
 
 const router = express.Router();
 
 router.use("/start", startRouter);
+router.use("/end", endRouter);
 
 export default router;
