207d0d394353a574bc40d13028946c9647fe41da
kishan1735 <gswwarriorsnba@gmail.com>
2025-09-16 14:06:04 +0530

    fix: schema fixes

--- server/src/config/db/schema/allocationRelations.ts
+++ server/src/config/db/schema/allocationRelations.ts
@@ -5,43 +5,49 @@ import { semester } from "./allocation.ts";
 import { users } from "./admin.ts";
 import { masterAllocation, allocationSection } from "./allocation.ts";
 
-
 export const allocationRelations = relations(allocation, ({ one }) => ({
     course: one(course, {
         fields: [allocation.courseCode],
-        references: [course.code]
+        references: [course.code],
     }),
     instructor: one(users, {
         fields: [allocation.instructorEmail],
-        references: [users.email]
+        references: [users.email],
     }),
     semester: one(semester, {
         fields: [allocation.semesterId],
-        references: [semester.id]
+        references: [semester.id],
     }),
 }));
 
 export const semesterRelations = relations(semester, ({ one }) => ({
     dcaConvenerAtStartOfSem: one(users, {
-        fields: [semester.dcaConvenerAtStartOfSemEmail], 
-        references: [users.email] 
+        fields: [semester.dcaConvenerAtStartOfSemEmail],
+        references: [users.email],
     }),
     hodAtStartOfSem: one(users, {
-        fields: [semester.hodAtStartOfSemEmail], 
-        references: [users.email] 
+        fields: [semester.hodAtStartOfSemEmail],
+        references: [users.email],
     }),
 }));
 
-export const masterAllocationRelations = relations(masterAllocation, ({ one, many}) => ({
-    ic: one(users, {
-        fields: [masterAllocation.ic],
-        references: [users.email]
-    }),
-    lecture: many(allocationSection),
-    tutorial: many(allocationSection),
-    practical: many(allocationSection)
+export const masterAllocationRelations = relations(
+    masterAllocation,
+    ({ one, many }) => ({
+        ic: one(users, {
+            fields: [masterAllocation.ic],
+            references: [users.email],
+        }),
+        course: one(course, {
+            fields: [masterAllocation.courseCode],
+            references: [course.code],
+        }),
+        lecture: many(allocationSection),
+        tutorial: many(allocationSection),
+        practical: many(allocationSection),
+    })
+);
+
+export const sectionRelations = relations(allocationSection, ({ many }) => ({
+    instructorEmails: many(users),
 }));
-
-
-    
-        
