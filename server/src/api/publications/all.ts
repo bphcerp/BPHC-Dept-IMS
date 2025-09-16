7c4e93449bb4d3842aa12364599c8046eab4f1d7
Samir Khairati <samirk10101@gmail.com>
2025-09-17 01:34:43 +0530

    style(analytics): formatting

--- server/src/api/publications/all.ts
+++ server/src/api/publications/all.ts
@@ -8,7 +8,7 @@ import {
     researgencePublications,
 } from "@/config/db/schema/publications.ts";
 import { eq, isNull } from "drizzle-orm";
-import { publicationsSchemas } from "lib";
+import type { publicationsSchemas } from "lib";
 
 const router = express.Router();
 
@@ -17,48 +17,53 @@ router.get(
     checkAccess(),
     asyncHandler(async (_req, res) => {
         const data: publicationsSchemas.ValidatedResponse = {
-            validated: (await db.select()
-                .from(researgencePublications)
-                .innerJoin(
-                    publicationsTable,
-                    eq(
-                        researgencePublications.publicationTitle,
-                        publicationsTable.title,
-                    ),
-                )).map((row) => row.researgence),
+            validated: (
+                await db
+                    .select()
+                    .from(researgencePublications)
+                    .innerJoin(
+                        publicationsTable,
+                        eq(
+                            researgencePublications.publicationTitle,
+                            publicationsTable.title
+                        )
+                    )
+            ).map((row) => row.researgence),
 
-            nonValidated: (await db.select()
-                .from(publicationsTable)
-                .leftJoin(
-                    researgencePublications,
-                    eq(
-                        researgencePublications.publicationTitle,
-                        publicationsTable.title,
-                    ),
-                )
-                .where(isNull(researgencePublications.authors)
-            ))
-            .map((row) => row.publications)
+            nonValidated: (
+                await db
+                    .select()
+                    .from(publicationsTable)
+                    .leftJoin(
+                        researgencePublications,
+                        eq(
+                            researgencePublications.publicationTitle,
+                            publicationsTable.title
+                        )
+                    )
+                    .where(isNull(researgencePublications.authors))
+            ).map((row) => row.publications),
         };
         console.log(data);
         res.status(200).json(data);
-    }),
+    })
 );
 
 router.get(
     "/",
     checkAccess(),
     asyncHandler(async (_req, res) => {
-        const response : publicationsSchemas.PublicationResponse = await  db.select().from(publicationsTable);
+        const response: publicationsSchemas.PublicationResponse = await db
+            .select()
+            .from(publicationsTable);
         res.status(200).json(response);
-    }),
+    })
 );
 
 router.get(
     "/meta/",
     checkAccess(),
     asyncHandler(async (_req, res) => {
-
         const allData = await db
             .select({
                 publication: publicationsTable,
@@ -72,12 +77,12 @@ router.get(
                 publicationsTable,
                 eq(
                     authorPublicationsTable.citationId,
-                    publicationsTable.citationId,
-                ),
+                    publicationsTable.citationId
+                )
             );
 
-        if(!allData.length) {
-            res.status(200).json([]); 
+        if (!allData.length) {
+            res.status(200).json([]);
             return;
         }
 
@@ -89,29 +94,28 @@ router.get(
         for (const row of allData) {
             const pub = row.publication;
             const coAuthor: publicationsSchemas.CoAuthor = {
-                                authorId: row.authorId,
-                                authorName: row.authorName,
-                            };
+                authorId: row.authorId,
+                authorName: row.authorName,
+            };
             if (!publicationsMap.has(pub.citationId)) {
                 publicationsMap.set(pub.citationId, {
                     ...pub,
                     status: row.status ?? null,
                     comments: row.comments ?? null,
-                    coAuthors: [coAuthor]
+                    coAuthors: [coAuthor],
                 });
             } else {
-                publicationsMap
-                    .get(pub.citationId)!
-                    .coAuthors.push({
-                        authorId: row.authorId,
-                        authorName: row.authorName
-                    });
+                publicationsMap.get(pub.citationId)!.coAuthors.push({
+                    authorId: row.authorId,
+                    authorName: row.authorName,
+                });
             }
         }
 
-        const data : publicationsSchemas.PublicationWithMetaResponse = Array.from(publicationsMap.values());
+        const data: publicationsSchemas.PublicationWithMetaResponse =
+            Array.from(publicationsMap.values());
         res.status(200).json(data);
-    }),
+    })
 );
 
 export default router;
