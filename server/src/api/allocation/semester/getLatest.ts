89f3e9902fc49ccec5fb96fc81e5ce569e27586a
catfo435 <pranavlsgreat210120@gmail.com>
2025-10-14 20:27:36 +0530

    fix: removed unused import

--- server/src/api/allocation/semester/getLatest.ts
+++ server/src/api/allocation/semester/getLatest.ts
@@ -1,6 +1,5 @@
 import db from "@/config/db/index.ts";
 import { HttpCode, HttpError } from "@/config/errors.ts";
-import { checkAccess } from "@/middleware/auth.ts";
 import { asyncHandler } from "@/middleware/routeHandler.ts";
 import express from "express";
 import { getLatestSemesterQuerySchema } from "node_modules/lib/src/schemas/Allocation.ts";
