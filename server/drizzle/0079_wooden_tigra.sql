9f4b139583845b14a70bdb537421c45ea79c4a96
Sanchay <sanchaynts9422@gmail.com>
2025-10-26 11:29:16 +0530

    chore: migration

--- server/drizzle/0079_wooden_tigra.sql
+++ server/drizzle/0079_wooden_tigra.sql
@@ -3,5 +3,5 @@ ALTER TABLE "phd_proposals" drop column "active";--> statement-breakpoint
 ALTER TABLE "phd_proposals" ADD COLUMN "active" boolean GENERATED ALWAYS AS (CASE WHEN status IN('deleted', 'completed', 'rejected')THEN NULL ELSE true END) STORED;
 
 ALTER TYPE "public"."phd_proposal_status" ADD VALUE 'draft_expired';--> statement-breakpoint
--- ALTER TABLE "phd_proposals" drop column "active";--> statement-breakpoint
--- ALTER TABLE "phd_proposals" ADD COLUMN "active" boolean GENERATED ALWAYS AS (CASE WHEN status IN('deleted', 'completed', 'draft_expired', 'rejected')THEN NULL ELSE true END) STORED;
+ALTER TABLE "phd_proposals" drop column "active";--> statement-breakpoint
+ALTER TABLE "phd_proposals" ADD COLUMN "active" boolean GENERATED ALWAYS AS (CASE WHEN status IN('deleted', 'completed', 'draft_expired', 'rejected')THEN NULL ELSE true END) STORED;
