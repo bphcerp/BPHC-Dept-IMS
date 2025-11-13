713d7e4927c124a1400000d271f5849cf432bbc9
Sanchay <sanchaynts9422@gmail.com>
2025-11-13 15:39:49 +0530

    chore: migration

---
+++ server/drizzle/0088_yielding_eternity.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "phd" ADD COLUMN "dac_member_1_email" text;--> statement-breakpoint
+ALTER TABLE "phd" ADD COLUMN "dac_member_1_name" text;--> statement-breakpoint
+ALTER TABLE "phd" ADD COLUMN "dac_member_2_email" text;--> statement-breakpoint
+ALTER TABLE "phd" ADD COLUMN "dac_member_2_name" text;
